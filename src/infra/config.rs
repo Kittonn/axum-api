ef489214c77b11e391fa21ae7dbd064694779b68
Kittipod Lambangchang <tonkittipod@gmail.com>
2026-01-31 02:24:16 +0700

    feat: add AppConfig for environment-based configuration

--- src/infra/config.rs
+++ src/infra/config.rs
@@ -1,1 +1,16 @@
+use std::env;
 
+pub struct AppConfig {
+    pub port: u16,
+}
+
+impl AppConfig {
+    pub fn from_env() -> Self {
+        let port = env::var("PORT")
+            .unwrap_or("4001".to_string())
+            .parse()
+            .expect("PORT must be a number");
+
+        Self { port }
+    }
+}
