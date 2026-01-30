5536b08b6625dd192ed42c6a45b6295c69a715c1
Kittipod Lambangchang <tonkittipod@gmail.com>
2026-01-31 01:50:27 +0700

    feat: setup axum

---
+++ src/infra/app.rs
@@ -0,0 +1,26 @@
+use axum::{
+    Router,
+    http::{
+        Method,
+        header::{AUTHORIZATION, CONTENT_TYPE},
+    },
+    routing::get,
+};
+use tower_http::cors::CorsLayer;
+
+pub fn create_app() -> Router {
+    let cors = CorsLayer::new()
+        .allow_methods([
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::PATCH,
+            Method::DELETE,
+        ])
+        .allow_credentials(true)
+        .allow_headers([CONTENT_TYPE, AUTHORIZATION]);
+
+    Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(cors)
+}
